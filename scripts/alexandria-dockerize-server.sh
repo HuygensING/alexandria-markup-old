b9d2d13add5de0db874183e5d42fc3e5a7f19823
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2017-03-27 14:54:21 +0200

    [NLA-357] Fix Dockerfile

--- scripts/alexandria-dockerize-server.sh
+++ scripts/alexandria-dockerize-server.sh
@@ -1,4 +1,4 @@
 mvn package && \
   export version=$(xmllint --xpath "/*[local-name()='project']/*[local-name()='version']/text()" pom.xml) && \
-  cd alexandria-markup-server && \
-    docker build -t huygensing/alexandria-markup-server -t huygensing/alexandria-markup-server:$version --build-arg version=$version .
+  cd alexandria-markup-xml-server && \
+    docker build -t huygensing/alexandria-markup-xml-server -t huygensing/alexandria-markup-xml-server:$version --build-arg version=$version .
